634ae0afefbe76346af0d4a84e73210c79ec485b
U1166 <morganeizard98@gmail.com>
2023-06-12 11:29:22 +0200

    ajout de Man.R plus résolution de probleme dans NewMan.R

--- Man.R
+++ Man.R
@@ -1,29 +1,56 @@
-setwd("/home/u1166/Documents/New")
 library(qqman)
 library(officer)
-simple <- read.table("covSPPP.assoc.logistic", header = TRUE)
-dom <- read.table("covSPPP_D.assoc.logistic", header = TRUE)
-rec <- read.table("covSPPP_R.assoc.logistic", header = TRUE)
+library("rtracklayer")
+library("GenomicRanges")
 
-add_sim <- simple[simple$TEST == "ADD",]
-add_sim <- na.omit(add_sim)
-add_dom <- dom[dom$TEST == "DOM",]
-add_dom <- na.omit(add_dom)
-add_rec <- rec[rec$TEST == "REC",]
-add_rec <- na.omit(add_rec)
-top_all <- add_sim[add_sim$P <=1e-05,]
-top_dom <- add_dom[add_dom$P <=1e-05,]
-top_rec <- add_rec[add_rec$P <=1e-05,]
+download.file("https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_19/gencode.v19.annotation.gtf.gz",destfile="./gencode.v19.annotation.gtf.gz")
+system("gunzip --keep --force gencode.v19.annotation.gtf.gz")
+Exons<-rtracklayer::readGFF("gencode.v19.annotation.gtf.gz")
+Exons$seqid<-gsub("^chr","",Exons$seqid)
 
-pdf("Manhattan_Cov.pdf", width=29.7/2,height=21/2)
-manhattan(add_sim, ylim = c(0,10), annotatePval = 1e-05, main = "Allelic")
-qq(add_sim$P)
-manhattan(add_dom, ylim = c(0,10), annotatePval = 1e-05, main = "Dominant")
-qq(add_dom$P)
-manhattan(add_rec, ylim = c(0,10), annotatePval = 1e-05, main = "Recessive")
-qq(add_rec$P)
-dev.off()
 
+Genes<-Exons[Exons$type=="gene",]
+Genes<-Genes[Genes$gene_type=="protein_coding",]
+Genes.GR<-makeGRangesFromDataFrame(Genes,keep.extra.columns=TRUE)
+
+model <- read.table("LAST.model", header = TRUE )
+donnee <- read.table("LAST.assoc.fisher", header = TRUE )
+mod <- merge(model, subset(donnee, select = c("SNP", "BP")), all.x = TRUE)
+
+man <- function(data){
+  all <- data[data$TEST == "ALLELIC",]
+  dom <- data[data$TEST == "DOM",]
+  rec <- data[data$TEST == "REC",]
+  geno <- data[data$TEST == "GENO",]
+  
+  pdf("Manhattan_OUT_1.pdf", width=29.7/2,height=21/2)
+  manhattan(all, main = "Allelic")
+  qq(all$P)
+  dev.off()
+  pdf("Manhattan_OUT_2.pdf", width=29.7/2,height=21/2)
+  manhattan(dom, main = "Dominant")
+  qq(dom$P)
+  dev.off()
+  pdf("Manhattan_OUT_3.pdf", width=29.7/2,height=21/2)
+  manhattan(rec, main = "Recessive")
+  qq(rec$P)
+  dev.off()
+  pdf("Manhattan_OUT_4.pdf", width=29.7/2,height=21/2)
+  manhattan(geno, main = "Genomic ")
+  qq(geno$P)
+  dev.off()
+}
+
+man(mod)
+
+all <- mod[mod$TEST == "ALLELIC",]
+dom <- mod[mod$TEST == "DOM",]
+rec <- mod[mod$TEST == "REC",]
+geno <- mod[mod$TEST == "GENO",]
+top_all <- all[all$P <=1e-05,]
+top_dom <- dom[dom$P <=1e-05,]
+top_rec <- rec[rec$P <=1e-05,]
+top_geno <- geno[geno$P <=1e-05,]
 
 plot_graph <- function(dt, snp, cadre){   #dt = data.frame, snp = chr
   signal <- dt[dt$SNP == snp,]
@@ -54,12 +81,12 @@ plot_graph <- function(dt, snp, cadre){   #dt = data.frame, snp = chr
   #rownames(mat) <- c("A1", "A2")
   
   ##### Manhattan #####
-  #pdf(paste0(dt[dt$SNP == snp,]$TEST, "_", snp, "_Manhattan_Plot_",".pdf"), width=29.7/2,height=21/2)
+  pdf(paste0(dt[dt$SNP == snp,]$TEST, "_", snp, "_Manhattan_Plot_",".pdf"))
   plot(-log10(smol_dt$P) ~ smol_dt$BP, xlab = "BP", 
        ylab = "-log10(p)", pch = 19, col = smol_dt$col, 
        main = "Manhattan plot",ylim = c(-5,10))
   text(signal$BP, -log10(signal$P)+0.5, signal$SNP)
-  abline(h = -log10(5e-08), col = 'red')
+  abline(h = -log10(8e-08), col = 'red')
   abline(h = -log10(1e-05), col = 'blue')
   if(length(GenesInRegion.GR.FWD)>0){
     rect(
@@ -88,10 +115,9 @@ plot_graph <- function(dt, snp, cadre){   #dt = data.frame, snp = chr
       pos=3,
       srt = 90)
   }
-  #dev.off()
+  dev.off()
   return()
 } 
-
 
 for (snp in top_all$SNP){
   plot_graph(all, snp, 1000000)
@@ -101,4 +127,7 @@ for (snp in top_dom$SNP){
 }
 for (snp in top_rec$SNP){
   plot_graph(rec, snp, 1000000)
+}
+for (snp in top_geno$SNP){
+  plot_graph(geno, snp, 1000000)
 }
